40444fc95b053422f90e95b797f761274df92380
ZannatulMim <zannatulmim980@gmail.com>
2023-11-23 21:31:11 +0600

    first commit

---
+++ operation.js
@@ -0,0 +1,15 @@
+var onionPrice = 20;
+var eggPrice = 10;
+console.log(onionPrice);
+console.log(eggPrice);
+var totalPrice = onionPrice + eggPrice;
+console.log(totalPrice);
+
+var priceDifference = onionPrice - eggPrice;
+console.log(priceDifference);
+
+var orangePrice = 20;
+var quantity = 7;
+
+var totalCost = orangePrice * quantity;
+console.log(totalCost);
---
+++ shorthand.js
@@ -0,0 +1,8 @@
+var tomatoPrice = 25;
+tomatoPrice = 30;
+
+var price1 = 30;
+var price2 = 10;
+
+price1 = price1 + 10;
+console.log(price1);
